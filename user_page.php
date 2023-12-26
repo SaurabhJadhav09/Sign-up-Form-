4ce0a96085cc4a2d0d199c9fe15944db277ba522
SaurabhJadhav09 <139687247+SaurabhJadhav09@users.noreply.github.com>
2023-12-26 20:57:42 +0530

    Update user_page.php

--- user_page.php
+++ user_page.php
@@ -13,6 +13,7 @@ if(!isset($_SESSION['user_name'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+   <h1>author - saurabh jadhav</h1>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
